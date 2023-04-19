f50dc0903526743a3ea3af62d7024a8fd9d0429f
Michael Browne <brownemm@bc.edu>
2023-04-19 13:08:49 -0400

    Initial Commit

---
+++ nba/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  nba
+//
+//  Created by Michael Browne on 4/19/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ nba/nbaApp.swift
@@ -0,0 +1,17 @@
+//
+//  nbaApp.swift
+//  nba
+//
+//  Created by Michael Browne on 4/19/23.
+//
+
+import SwiftUI
+
+@main
+struct nbaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
